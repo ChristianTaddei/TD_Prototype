2db9bc643c32b0aa47a2a38afda43a2325c9c5d8
Midkar <christian.taddei@student.supsi.ch>
2021-06-04 18:18:36 +0200

    Add SimulationManager

--- Assets/Scripts/Game/GameManager.cs
+++ Assets/Scripts/Game/GameManager.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public InterfaceManager InterfaceManager;
+    public SimulationManager SimulationManager;
+
     BoardRepresentation br;
 
     void Start()
@@ -12,19 +14,24 @@ public class GameManager : MonoBehaviour
         // TODO: add components here to have correct order
 
         Surface surface = new Surface(10.0f);
+        SimulationManager = new SimulationManager(surface);
 
         InterfaceManager = new InterfaceManager(
             new ModifyTerrainCommand(surface)
-            );
+        );
 
+        // TODO: make into repres & manager
         Board board = new Board(surface);
         br = BoardRepresentation.MakeFrom(board);
 
+        // Register observers/observables
     }
 
     void Update()
     {
         InterfaceManager.Update();
+        SimulationManager.Update();
+
         br.Sync();
     }
 }
---
+++ Assets/Scripts/Simulation/SimulationManager.cs
@@ -0,0 +1,14 @@
+public class SimulationManager
+{
+    private Surface surface;
+
+    public SimulationManager(Surface surface)
+    {
+        this.surface = surface;
+    }
+
+    public void Update()
+    {
+
+    }
+}
