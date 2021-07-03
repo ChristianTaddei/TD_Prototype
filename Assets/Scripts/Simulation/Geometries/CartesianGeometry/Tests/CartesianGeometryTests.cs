a008cede137239a7f3ffd3ccbe50fc560c380a4d
Midkar <christian.taddei@student.supsi.ch>
2021-07-03 23:06:20 +0200

    CartesianGeometryTest uses internal constructor

--- Assets/Scripts/Simulation/Geometries/CartesianGeometry/Tests/CartesianGeometryTests.cs
+++ Assets/Scripts/Simulation/Geometries/CartesianGeometry/Tests/CartesianGeometryTests.cs
@@ -10,14 +10,11 @@ namespace Tests
 	public class CartesianGeometryTests
 	{
 		CartesianGeometry cartesianGeometry;
-		Mock<Factory> cartesianFactory;
 
 		[SetUp]
 		public void Setup()
 		{
 			cartesianGeometry = new CartesianGeometry();
-
-			cartesianFactory = new Mock<Factory>();
 		}
 
 		[TearDown]
@@ -26,7 +23,6 @@ namespace Tests
 
 		}
 
-		// TODO: to geometry too
 		[Test]
 		public void areCloserThanTollerance_vectorsCloser_true()
 		{
@@ -58,48 +54,48 @@ namespace Tests
 		public void TestProject()
 		{
 			CartesianTriangle Plane_Oz = new CartesianTriangle(
-				new CartesianVector(new Vector3(0, 0, 0)), // TODO: this should be automatic, other way rather than specific CV constructor?
-				new CartesianVector(new Vector3(1, 0, 0)),
-				new CartesianVector(new Vector3(0, 1, 0)));
+				new CartesianVector(0, 0, 0), // TODO: this should be automatic, other way rather than specific CV constructor?
+				new CartesianVector(1, 0, 0),
+				new CartesianVector(0, 1, 0));
 
-			CartesianVector cv = new CartesianVector(new Vector3(0, 0, 1));
+			CartesianVector cv = new CartesianVector(0, 0, 1);
 
 			Assert.AreEqual(new Vector3(0, 0, 0), cartesianGeometry.Project(cv, Plane_Oz).FloatRepresentation);
 
 			CartesianTriangle ShiftedPlane_Oz = new CartesianTriangle(
-				new CartesianVector(new Vector3(0, 0, 1)),
-				new CartesianVector(new Vector3(1, 0, 1)),
-				new CartesianVector(new Vector3(0, 1, 1)));
+				new CartesianVector(0, 0, 1),
+				new CartesianVector(1, 0, 1),
+				new CartesianVector(0, 1, 1));
 
-			cv = new CartesianVector(new Vector3(0, 0, 0));
+			cv = new CartesianVector(0, 0, 0);
 
 			Assert.AreEqual(new Vector3(0, 0, 1), cartesianGeometry.Project(cv, ShiftedPlane_Oz).FloatRepresentation);
 
 			CartesianTriangle ShiftedPlane_Oy = new CartesianTriangle(
-				new CartesianVector(new Vector3(1, 1, 0)),
-				new CartesianVector(new Vector3(0, 1, 1)),
-				new CartesianVector(new Vector3(0, 1, 0)));
+				new CartesianVector(1, 1, 0),
+				new CartesianVector(0, 1, 1),
+				new CartesianVector(0, 1, 0));
 
-			cv = new CartesianVector(new Vector3(3, 3, 3));
+			cv = new CartesianVector(3, 3, 3);
 
 			Assert.AreEqual(new Vector3(3, 1, 3), cartesianGeometry.Project(cv, ShiftedPlane_Oy).FloatRepresentation);
 
-			cv = new CartesianVector(new Vector3(-3, -3, -3));
+			cv = new CartesianVector(-3, -3, -3);
 
 			Assert.AreEqual(new Vector3(-3, 1, -3), cartesianGeometry.Project(cv, ShiftedPlane_Oy).FloatRepresentation);
 
 			CartesianTriangle Plane_45degs = new CartesianTriangle(
-				new CartesianVector(new Vector3(0, 0, 0)),
-				new CartesianVector(new Vector3(1, 0, 1)),
-				new CartesianVector(new Vector3(0, 1, 1)));
+				new CartesianVector(0, 0, 0),
+				new CartesianVector(1, 0, 1),
+				new CartesianVector(0, 1, 1));
 
-			cv = new CartesianVector(new Vector3(0.5f, 0.5f, 1.0f));
+			cv = new CartesianVector(0.5f, 0.5f, 1.0f);
 			Assert.AreEqual(new Vector3(0.5f, 0.5f, 1.0f), cartesianGeometry.Project(cv, Plane_45degs).FloatRepresentation);
 
-			cv = new CartesianVector(new Vector3(-0.5f, -0.5f, 2.0f));
+			cv = new CartesianVector(-0.5f, -0.5f, 2.0f);
 			Assert.AreEqual(new Vector3(0.5f, 0.5f, 1.0f), cartesianGeometry.Project(cv, Plane_45degs).FloatRepresentation);
 
-			cv = new CartesianVector(new Vector3(0.0f, 0.0f, 1.5f));
+			cv = new CartesianVector(0.0f, 0.0f, 1.5f);
 			Assert.AreEqual(new Vector3(0.5f, 0.5f, 1.0f), cartesianGeometry.Project(cv, Plane_45degs).FloatRepresentation);
 		}
 	}
