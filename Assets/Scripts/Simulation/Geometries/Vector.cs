341e73cf3d2eb389b1be1c2ba3e17a2c5047e2d7
Midkar <christian.taddei@student.supsi.ch>
2021-07-03 20:59:36 +0200

    Temporarily reintroduced overridden Equals for Vectors
    Test must be modified to use new geometry method instead before removing.

--- Assets/Scripts/Simulation/Geometries/Vector.cs
+++ Assets/Scripts/Simulation/Geometries/Vector.cs
@@ -8,4 +8,16 @@ using UnityEngine;
 public abstract class Vector /* : Summable<Vector>, Crossable<Vector>, Dottable<Vector> */
 {
 	public abstract Vector3 FloatRepresentation { get; }
+
+	// TODO: remove equals and hashcode, but some test still use it
+	public sealed override bool Equals(object otherObject)
+	{
+		return otherObject is Vector otherPoint
+		 && Vector3.Distance(this.FloatRepresentation, otherPoint.FloatRepresentation) < 0.0001f;
+	}
+
+	public override int GetHashCode()
+	{
+		return -509336368 + FloatRepresentation.GetHashCode();
+	}
 }
